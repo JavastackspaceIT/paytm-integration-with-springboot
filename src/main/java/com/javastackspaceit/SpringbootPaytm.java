02453ebb976d3b40959a7853f3937f0e5753d145
rksinghyadav1995@gmail.com <rksinghyadav1995@gmail.com>
2021-02-02 21:23:07 +0530

    initial commit

---
+++ src/main/java/com/javastackspaceit/SpringbootPaytm.java
@@ -0,0 +1,12 @@
+package com.javastackspaceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPaytm {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPaytm.class, args);
+	}
+}
